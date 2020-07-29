6e2eee079799ff996a81cbc676ee7966fe1531b5
Riki Bando <ricky1231@g.ecc.u-tokyo.ac.jp>
2020-07-30 01:06:03 +0900

    rename datetime client

--- datetime-client.c
+++ datetime-client.c
@@ -3,31 +3,34 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <errno.h>
 
 #define BUFFSIZE 2048
 
 int main(int argc, char *argv[]){
-    int destport, INET_PROTOCL, sock, s, n;
-    char deststr[32], buf[BUFFSIZE];
+    int INET_PROTOCL, sock, s, n;
+    char destip[100], destport[100], buf[BUFFSIZE];
 
     if(argc < 2){
-        printf("Please specify dest address as argv[1].\n");
+        printf("Please specify dest address.\n");
         return 0;
     }
 
-    strcpy(deststr, argv[1]);
-    printf("Dest address: %s\n", deststr);
+    strcpy(destip, argv[1]);
+    printf("Dest address: %s\n", destip);
 
-    destport = 13; // datetime protocol
-    printf("Dest port number: %d\n", destport);
+    strcpy(destport, "13");
+    printf("Dest port number: %s\n", destport);
 
-    char *p = strrchr(deststr, ':');
+    char *p = strrchr(destip, ':');
 
     if(p == NULL){
         struct sockaddr_in server;
         server.sin_family = AF_INET;
-        server.sin_port = htons(destport);
+        server.sin_port = htons(atoi(destport));
         sock = socket(AF_INET, SOCK_STREAM, 0);
 
         if (sock < 0) {
@@ -35,7 +38,7 @@ int main(int argc, char *argv[]){
             return 0;
         }
 
-        s = inet_pton(AF_INET, deststr, &server.sin_addr);
+        s = inet_pton(AF_INET, destip, &server.sin_addr);
 
         connect(sock, (struct sockaddr *)&server,
                 sizeof(server));
@@ -43,7 +46,7 @@ int main(int argc, char *argv[]){
     else {
         struct sockaddr_in6 server;
         server.sin6_family = AF_INET6;
-        server.sin6_port = htons(destport);
+        server.sin6_port = htons(atoi(destport));
         sock = socket(AF_INET6, SOCK_STREAM, 0);
 
         if (sock < 0) {
@@ -51,7 +54,7 @@ int main(int argc, char *argv[]){
             return 0;
         }
 
-        s = inet_pton(AF_INET6, deststr, &server.sin6_addr);
+        s = inet_pton(AF_INET6, destip, &server.sin6_addr);
 
         connect(sock, (struct sockaddr *)&server,
                 sizeof(server));
